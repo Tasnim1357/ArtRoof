17a61164f4f6b99854f2e616c4147cf9eaad1575
DELL <bellhossain12@gmail.com>
2024-04-30 11:35:21 +0600

    styles added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -38,6 +38,16 @@ const Navbar = () => {
             <ul tabIndex={0} className="menu menu-sm dark:bg-black dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
            
         {Navlinks}
+        <li>
+         
+        <label className="cursor-pointer grid place-items-center w-12">
+  <input type="checkbox"  checked={theme === 'dark'} // Checked state based on the current theme
+        onChange={handleThemeToggle} value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+ 
+  <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" ></path></svg>
+  <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+</label>
+          </li>
         <li className='mb-5'>
         {
             user && <div className="tooltip p-0 ml-2" data-tip={user.displayName}>
@@ -67,9 +77,7 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1 dark:text-white">
           {Navlinks}
           <li>
-         {/* {
-          theme==='dark'? <div className=' bg-blue-600 hover:bg-blue-600 px-3'> <MdOutlineWbSunny className='bg-blue-600 text-3xl text-white' onClick={handleTheme} /></div>: <div > <FaMoon className=' text-2xl' onClick={handleTheme} /></div>
-         } */}
+        
         <label className="cursor-pointer grid place-items-center ">
   <input type="checkbox"  checked={theme === 'dark'} // Checked state based on the current theme
         onChange={handleThemeToggle} value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
